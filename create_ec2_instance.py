2e3cd66d9ea81e59c9c0c2997e65acaa8f8cb4f4
hanxiong1122 <hanxiong.wang1122@gmail.com>
2018-07-31 16:44:23 -0400

    save instance info as one json file

--- create_ec2_instance.py
+++ create_ec2_instance.py
@@ -1,5 +1,7 @@
 import boto3
 import time
+import json
+import os
 
 # def create_instance()
 
@@ -42,9 +44,24 @@ class create_ec2_instances(object):
 
 		print("We created ", running_instance_num, "running instances")
 
+		return self.save_info(filtered_instances)		
+		# return public_ip_addresses, instances_id
+
+	def save_info(self, filtered_instances):
 		for instance in filtered_instances:
 			print(instance.id, "public_ip_address is ", instance.public_ip_address)
-		return [instance.public_ip_address for instance in filtered_instances], [instance.id for instance in filtered_instances]
+		public_ip_addresses = [instance.public_ip_address for instance in filtered_instances]
+		instances_id = [instance.id for instance in filtered_instances]
+		instances_info = { 
+						"public_ip_addresses": public_ip_addresses,
+						"instances_id": instances_id
+						}
+
+		if not os.path.isdir("./tmp/"):
+			os.mkdir("./tmp")
+		with open("./tmp/instances_info.json",'w') as outfile:
+			json.dump(instances_info, outfile, indent = 4 , sort_keys = True)
+		return public_ip_addresses, instances_id
 
 	def terminate(self):
 		if (self.id_list == []):
@@ -67,7 +84,7 @@ class create_ec2_instances(object):
 
 if __name__=="__main__":
 	ImageId = 'ami-d87877a7'
-	instances = create_ec2_instances(ImageId = ImageId, MaxCount = 2, MinCount = 1, InstanceType = 'm4.xlarge')
+	instances = create_ec2_instances(ImageId = ImageId, MaxCount = 2, MinCount = 1)#, InstanceType = 'm4.xlarge')
 	print(instances.get_instance_public_ip())
 	print(instances.get_instance_id())
 	# time.sleep(10)
